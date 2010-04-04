c2578e63a4fed2be314201d789c0221e8303bef4
Ben Mabey <ben@benmabey.com>
2010-04-04 12:04:26 -0600

    adds bundler support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,16 @@
-require 'rubygems'
+require "rubygems"
+require "bundler"
+Bundler.setup
+
 require 'spec'
 require 'active_record'
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'database_cleaner'
 
+
+
 Spec::Runner.configure do |config|
-  
 end
 
 alias running lambda
